7b0eea2012e604dde8bd914307c0ce113ec80b43
Dan <35927536+dan-mba@users.noreply.github.com>
2022-07-09 12:14:50 -0400

    Working Scraper

---
+++ scrape.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import pandas as pd
+import os
+
+cwd_path = os.getcwd()
+driver_path = os.path.join(cwd_path, 'bin', 'chromedriver')
+
+web = 'https://www.google.com/search?q=JavaScript&tbm=nws&tbs=qdr:w&dpr=1'
+
+options = Options()
+options.headless = True
+driver_service = Service(executable_path=driver_path)
+driver = webdriver.Chrome(service=driver_service, options=options)
+driver.get(web)
+
+containers = driver.find_elements(by='xpath', value='//*[@id="rso"]/div')
+
+titles = []
+sites = []
+links = []
+for container in containers:
+    title = container.find_element(by='xpath', value='././div/a/div/div[2]/div[2]').text.replace('|', '')
+    site = container.find_element(by='xpath', value='./div/a/div/div[2]/div[1]/span').text.replace('|', '')
+    link = container.find_element(by='xpath', value='./div/a').get_attribute('href')
+    titles.append(title)
+    sites.append(site)
+    links.append(link)
+
+my_dict = {'Title': titles, 'Website': sites, 'Link': links}
+df_headlines = pd.DataFrame(my_dict)
+df_headlines.to_markdown('news.md', index=False)
+
+driver.quit()
