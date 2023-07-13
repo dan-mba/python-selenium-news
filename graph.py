9e39ab6d9f852aeb9f97ef848c29753159099f6f
Dan <35927536+dan-mba@users.noreply.github.com>
2023-07-13 13:30:40 -0400

    improved graph

--- graph.py
+++ graph.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 df = pd.read_csv("./history.csv",parse_dates=['Date'], usecols=['Date', 'Website'])
 df = df[(df['Date'] > (datetime.today() - timedelta(days=7)))]
-ax = df.value_counts(subset=['Website']).plot.bar(rot=0)
+df = df.value_counts(subset=['Website']).reset_index().head()
+ax = df.plot(kind='bar',x='Website',rot=0, figsize=(20,10), xlabel='', fontsize=14)
 fig = ax.get_figure()
-fig.savefig('last-week.png')
+fig.savefig('last-week.png', dpi=64, bbox_inches='tight')
