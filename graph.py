a182c221720a986e966885904c98c7ad3319f01c
Dan <35927536+dan-mba@users.noreply.github.com>
2023-08-09 09:22:13 -0400

    limit csv file

--- graph.py
+++ graph.py
@@ -2,8 +2,13 @@ from datetime import datetime, timedelta
 import pandas as pd
 
 df = pd.read_csv("./history.csv",parse_dates=['Date'], usecols=['Date', 'Website'])
-df = df[(df['Date'] > (datetime.today() - timedelta(days=7)))]
-df = df.value_counts(subset=['Website']).reset_index().head()
-ax = df.plot(kind='bar',x='Website',rot=0, figsize=(20,10), xlabel='', fontsize=14)
+
+df_week = df[(df['Date'] > (datetime.today() - timedelta(days=7)))]
+df_week = df_week.value_counts(subset=['Website']).reset_index().head()
+ax = df_week.plot(kind='bar',x='Website',rot=0, figsize=(20,10), xlabel='', fontsize=14)
 fig = ax.get_figure()
 fig.savefig('last-week.png', dpi=64, bbox_inches='tight')
+
+# Limit csv to 180 days
+df = df[(df['Date'] > (datetime.today() - timedelta(days=180)))]
+df.to_csv('history.csv', index=False, header=True)
